2b62add3ab199d63da183c746bb72606cac2feb5
Charlie L <charliesbot@gmail.com>
2022-07-30 21:49:04 -0700

    migrate to node

---
+++ api/refreshToken.ts
@@ -0,0 +1,21 @@
+import type {VercelRequest, VercelResponse} from "@vercel/node";
+import {AuthorizationCode} from "simple-oauth2";
+import {feedlyConfig, baseConfig} from "../util/util";
+
+export default async (request: VercelRequest, response: VercelResponse) => {
+  const client = new AuthorizationCode(feedlyConfig);
+
+  try {
+    const accessToken = await client.getToken({
+      ...baseConfig,
+      code: request.query.code as string,
+    });
+    response.end(JSON.stringify(accessToken));
+
+  } catch (error) {
+    console.log('Access Token Error', error.message);
+  }
+};
+
+
+
