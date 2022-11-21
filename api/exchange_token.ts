c1f94ca50d7e5652cba5b7e7caa991932a27569e
Charlie L <charliesbot@gmail.com>
2022-11-20 16:09:10 -0800

    add refresh process

--- api/exchange_token.ts
+++ api/exchange_token.ts
@@ -1,21 +1,20 @@
 import type {VercelRequest, VercelResponse} from "@vercel/node";
 import {AuthorizationCode} from "simple-oauth2";
-import {feedlyConfig} from "../util/util";
+import {feedlyConfig, baseConfig} from "../util/util";
 
 export default async (request: VercelRequest, response: VercelResponse) => {
   const client = new AuthorizationCode(feedlyConfig);
 
   try {
-    const data = request.body;
-    let accessToken = client.createToken(JSON.parse(data));
-    accessToken = await accessToken.refresh();
+    const accessToken = await client.getToken({
+      ...baseConfig,
+      code: request.query.code as string,
+    });
     response.setHeader('Content-Type', 'application/json');
     response.end(JSON.stringify(accessToken));
+
   } catch (error) {
-    console.log('Refresh Token Error', error.message);
+    console.log('Access Token Error', error.message);
     response.end();
   }
 };
-
-
-
